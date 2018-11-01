5da3fa75a4711d444d08d5a1646bf8fe1fada8cd
Felix Brucker <felix@felixbrucker.com>
2018-11-01 21:42:22 +0100

    Add pledge info to bhd stats

--- wallets/bhd-wallet.js
+++ wallets/bhd-wallet.js
@@ -7,4 +7,23 @@ module.exports = class BHDWallet extends GenericWallet {
     wallet.removeUserAgent = true;
     super(wallet);
   }
+
+  async getDataForNewWallet() {
+    const result = await super.getDataForNewWallet();
+    const { result: pledgeData } = await this.postUrl(`http://${this.hostname}:${this.port}`, {
+      jsonrpc: '2.0',
+      id: 0,
+      method: 'getpledge',
+      params: [],
+    }, {
+      auth: {
+        username: this.user,
+        password: this.pass,
+      },
+    });
+    result.pledgeAmount = pledgeData.pledge;
+    result.pledgeCapacity = pledgeData.capacity;
+
+    return result;
+  }
 };
